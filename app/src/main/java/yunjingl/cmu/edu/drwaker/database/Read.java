f5496cf64df2305e2eb73c1366029ed3ab3a3cef
yunjing <mo13yu@hotmail.com>
2016-04-13 22:52:01 -0400

    update database and add two entities

--- app/src/main/java/yunjingl/cmu/edu/drwaker/database/Read.java
+++ app/src/main/java/yunjingl/cmu/edu/drwaker/database/Read.java
@@ -1,7 +1,25 @@
 package yunjingl.cmu.edu.drwaker.database;
 
+import java.util.ArrayList;
+
+import yunjingl.cmu.edu.drwaker.entities.Alarm;
+import yunjingl.cmu.edu.drwaker.entities.ClockTime;
+
 /**
  * Created by yunjing on 4/13/16.
  */
 public class Read {
+    public ClockTime readCalender(int id){
+
+        return new ClockTime();
+    }
+    public Alarm readAlarm(int id){
+        return new Alarm();
+    }
+    public ArrayList<ClockTime> readAllCalender(int id){
+        return new ArrayList<ClockTime>();
+    }
+    public ArrayList<Alarm> readAllAlarm(){
+        return new ArrayList<Alarm>();
+    }
 }
---
+++ app/src/main/java/yunjingl/cmu/edu/drwaker/entities/Location.java
@@ -0,0 +1,7 @@
+package yunjingl.cmu.edu.drwaker.entities;
+
+/**
+ * Created by yunjing on 4/13/16.
+ */
+public class Location {
+}
