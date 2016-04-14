c773e9046a52dc31745b65d4a3d8f131ad4d6985
danielppp <yapengli@Yapengs-MacBook.wv.cc.cmu.edu>
2016-04-13 21:05:44 -0400

    face_detector4

--- app/build/generated/source/r/debug/yunjingl/cmu/edu/drwaker/R.java
+++ app/build/generated/source/r/debug/yunjingl/cmu/edu/drwaker/R.java
@@ -2724,7 +2724,6 @@ containing a value of this type.
     }
     public static final class menu {
         public static final int menu_main=0x7f0e0000;
-        public static final int menu_maps_activity2=0x7f0e0001;
     }
     public static final class mipmap {
         public static final int ic_launcher=0x7f030000;
